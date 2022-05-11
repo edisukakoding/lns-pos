90147aae41458bd8089ff3f42594446e982ac85c
unknown <edi.sukakoding.com>
2022-05-12 00:51:19 +0700

    add crud departments

--- routes/master.php
+++ routes/master.php
@@ -8,5 +8,5 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('master')->group(function () {
     Route::get('/', [MasterController::class, 'index'])->name('layout.master');
     Route::post('/departments/data', [DepartmentController::class, 'data'])->name('departments.data');
-    Route::resource('departments', DepartmentController::class);
+    Route::resource('departments', DepartmentController::class)->except(['edit', 'show', 'create']);
 });
